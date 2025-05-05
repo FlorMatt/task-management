2eba941e587bdd18477ef5fe1926f8818f5c2124
FlorMatt <matthew.senti@smail.rasmussen.edu>
2025-05-05 17:30:09 -0500

    updated application.properties adding H2 Added UserRepository and updated the User Entity

--- src/main/java/com/floormatt/taskmanagement/models/User.java
+++ src/main/java/com/floormatt/taskmanagement/models/User.java
@@ -1,18 +1,27 @@
 package com.floormatt.taskmanagement.models;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
 @Data //generates the getters, setters, toString, equals, hashCode
+@Entity
+@Table(name = "users")
 @NoArgsConstructor //generates an empty constructor
 @AllArgsConstructor //generates constructor with all fields
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
     @NotBlank(message = "Username cannot be empty")
     private String username;
 
+    @Column(nullable = false)
     @NotBlank(message = "Password cannot be empty")
     private String password;
 }
